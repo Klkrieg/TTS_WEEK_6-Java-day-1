3d6d15e72755a1e6600ce5f6bc5653e99b41cc82
Karson Krieg <klkrieg3@gmail.com>
2021-01-22 17:37:19 -0600

    homework

--- src/com/tts/Main.java
+++ src/com/tts/Main.java
@@ -14,15 +14,19 @@ public class Main {
     * Paragraph comments
     *
     * */
+
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Hello World.");
-        System.out.print("Today is a good day for \n Java. \n");
-        System.out.print("Don't you think?\n");
-        System.out.println("What is your name?");
-        String firstName = input.nextLine();
-        System.out.printf("Hello, %s!\n", firstName);
-        System.out.printf("Goodbye %s!\n", firstName);
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Hello World.");
+//        System.out.print("Today is a good day for \n Java. \n");
+//        System.out.print("Don't you think?\n");
+//        System.out.println("What is your name?");
+//        String firstName = input.nextLine();
+//        System.out.printf("Hello, %s!\n", firstName);
+//        System.out.printf("Goodbye %s!\n", firstName);
+        int myInt = 2147483647;
+        System.out.println(myInt * 2);
 
     }
 }
--- src/com/tts/Second.java
+++ src/com/tts/Second.java
@@ -1,18 +1,37 @@
 package com.tts;
+import java.util.Locale;
 import java.util.Scanner;
 public class Second {
     public static void main(String[] args) {
-
         /*
         * Dear name,
         You are adverb adjective and I want to be your noun! I want to go to the nounTwo with you in number days.
         Sincerely, yourName
         * */
         Scanner input = new Scanner(System.in);
+        System.out.println("Want to play a game? (yes | no)");
+        String answer = input.nextLine();
+        if(answer.equalsIgnoreCase("yes")) {
+            System.out.println("Wonderful. We are playing a madlib! ");
+            System.out.println("First we need a name(not yours): ");
+            String name = input.nextLine();
+            System.out.println("Next we need an adverb: ");
+            String adverb = input.nextLine();
+            System.out.println("Next a verb: ");
+            Int verb = input.nextLine();
+            System.out.println("Next a noun: ");
+            String noun1 = input.nextLine();
+            System.out.println("Next another noun: ");
+            String #noun2 = input.nextLine();
+            System.out.println("Now we need a number: ");
+            String number = input.nextLine();
+            System.out.println("And finally Your name: ");
+            String yourName = input.nextLine();
+            System.out.printf("Dear %s,\n You are %s %s and I want to be your %s! I want to go to the %s with you in %s days.\n Sincerely, %s.", name, adverb, verb, noun1, noun2, number, yourName);
 
-        System.out.println("Hello, What's your name?");
-        String firstName = input.nextLine();
-        System.out.printf("Hello %s.\n", firstName);
-        input.close();
+        }else{
+            System.out.println("Well, see you later...");
+        }
+        System.exit(0);
+        }
     }
-}
